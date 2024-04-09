77627d851a68988a0b51190149e700459ef6c205
--delete <--delete>
2024-04-09 23:58:16 +0600

    add dynamic movie data

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -1,19 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
+    // console.log(movie);
     return (
         <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
             <Image className="w-full object-cover" src="/assets/movie-1.png" width={356} height={428} alt="" />
             <figcaption className="pt-4">
-                <h3 className="text-xl mb-1">Iron Man</h3>
-                <p className="text-[#575A6E] text-sm mb-2">Action/Adventure/Sci-fi</p>
+                <h3 className="text-xl mb-1">{movie?.original_title}</h3>
+                <p className="text-[#575A6E] text-sm mb-2">Genre ids: {movie.genre_ids.join(", ")}</p>
                 <div className="flex items-center space-x-1 mb-5">
-                    <Image src="/assets/star.svg" width={14} height={14} alt="" />
-                    <Image src="/assets/star.svg" width={14} height={14} alt="" />
-                    <Image src="/assets/star.svg" width={14} height={14} alt="" />
-                    <Image src="/assets/star.svg" width={14} height={14} alt="" />
-                    <Image src="/assets/star.svg" width={14} height={14} alt="" />
+                    {[...Array(Math.round(movie.vote_average / 2))].map((_, index) => (
+                        <Image key={index} src="/assets/star.svg" width={14} height={14} alt="" />
+                    ))}
                 </div>
                 <Link href="/" className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm">
                     <Image src="/assets/tag.svg" width={20} height={20} alt="" />
--- components/MovieContainer.jsx
+++ components/MovieContainer.jsx
@@ -1,14 +1,13 @@
 import MovieCard from "./MovieCard";
-
+import movieData from "@/movieData.json"
 const MovieContainer = () => {
+    // console.log(movieData);
     return (
         <div className="content">
             <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
+                {
+                    movieData?.map(movie => <MovieCard key={movie.id} movie={movie} />)
+                }
             </div>
         </div>
     );
